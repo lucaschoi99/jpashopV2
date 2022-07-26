5ae72e8d8f07ec64850578c5a4e0d9eb9bc21e02
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-07-26 09:36:18 +0900

    Initial commit

---
+++ src/main/java/jpamarket/jpashopV2/JpashopV2Application.java
@@ -0,0 +1,13 @@
+package jpamarket.jpashopV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/jpamarket/jpashopV2/JpashopV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpamarket.jpashopV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
