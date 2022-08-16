d578ff6619b43d5ab9b9fca101e205bdb7c47012
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-08-16 18:30:04 +0900

    MemberService 계층 개발 (회원가입, 회원조회 기능)

---
+++ src/main/java/jpamarket/jpashopV2/service/MemberService.java
@@ -0,0 +1,43 @@
+package jpamarket.jpashopV2.service;
+
+import jpamarket.jpashopV2.domain.Member;
+import jpamarket.jpashopV2.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원가입
+    public Long join(Member member) {
+        checkDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    private void checkDuplicateMember(Member member) {
+        List<Member> dupMembers = memberRepository.findByName(member.getName());
+        if (!dupMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 전체회원 조회
+    public List<Member> findAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    // 회원 단건 조회
+    public Member findMember(Long id) {
+        return memberRepository.findMember(id);
+    }
+
+
+
+}
