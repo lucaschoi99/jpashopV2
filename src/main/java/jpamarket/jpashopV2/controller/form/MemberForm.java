0f629ff175ffa1d6623dcdbf99ea2eaafe2c1c33
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-08-18 23:56:57 +0900

    MemberForm 멤버 회원가입 class

---
+++ src/main/java/jpamarket/jpashopV2/controller/form/MemberForm.java
@@ -0,0 +1,17 @@
+package jpamarket.jpashopV2.controller.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter @Setter
+public class MemberForm {
+
+    @NotEmpty(message = "회원 이름은 필수 항목입니다.")
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
