d5d18a971fc81911716d6dad5074816f9d1e2f28
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-03-15 17:01:41 +0900

    feat: 상품 등록 form postmapping 메서드 추가
    More details:
    - 등록된 상품 form 정보로 item 저장.

--- src/main/java/jpamarket/jpashopV2/controller/ItemController.java
+++ src/main/java/jpamarket/jpashopV2/controller/ItemController.java
@@ -1,14 +1,21 @@
 package jpamarket.jpashopV2.controller;
 
+import jakarta.validation.Valid;
 import jpamarket.jpashopV2.controller.form.ItemForm;
+import jpamarket.jpashopV2.domain.Category;
+import jpamarket.jpashopV2.domain.Item;
 import jpamarket.jpashopV2.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class ItemController {
 
     private final ItemService itemService;
@@ -18,4 +25,22 @@ public class ItemController {
         model.addAttribute("form", new ItemForm());
         return "items/createItemForm";
     }
+
+    @PostMapping(value = "items/new")
+    public String postRegisterItem(@Valid ItemForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "items/createItemForm";
+        }
+        Item item = Item.builder()
+                .category(form.getCategory())
+                .name(form.getName())
+                .price(form.getPrice())
+                .amount(form.getAmount())
+                .build();
+        log.info(">>> Item Category={}", item.getCategory());
+
+        itemService.saveItem(item);
+        return "redirect:/";
+    }
+
 }
